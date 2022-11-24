92276f00a0c6ff87cc7109c93bd9fa838d389864
Diego Viana <dsantos@inf.ufsm.br>
2022-11-24 20:55:24 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,23 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  headers: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "http://localhost:3000, https://myblocklymaze.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "*",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
