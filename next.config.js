b429b5d607e8bb4e8d847ec9cecf6e64835258e2
Diego Viana <dsantos@inf.ufsm.br>
2022-11-24 22:00:15 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,11 @@ const nextConfig = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "*",
+            value: "http://localhost:3000",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://myblocklymaze.vercel.app",
           },
           {
             key: "Access-Control-Allow-Methods",
