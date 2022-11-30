0c098502b304f3a0b9bf887267a39c836136e088
Diego Viana <dsantos@inf.ufsm.br>
2022-11-29 21:56:25 -0300

    remoção da coluna created_at_pt_br em mazes

---
+++ prisma/migrations/20221130004721_drop_created_at_pt_br_em_mazes/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `created_at_pt_br` on the `mazes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "mazes" DROP COLUMN "created_at_pt_br";
