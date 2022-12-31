47f8abf5d528b49d3e50c4a31f5986e0c3bb0df4
Diego Viana <dsantos@inf.ufsm.br>
2022-12-31 17:04:06 -0300

    resizing the image before sending it to firebase

--- utils/takePrintscreen.js
+++ utils/takePrintscreen.js
@@ -1,20 +1,32 @@
 import captureWebsite from "capture-website";
+const sharp = require("sharp");
 import upFireThumbnail from "./upFireThumbnail";
 
 const takePrintscreen = async (levels, url_image) => {
+  let bufferNewDimension;
+
   const mazeGameUrl =
     "https://myblocklymaze-game.vercel.app/maze.html?levels=" +
     levels +
     "&url_image=" +
     url_image +
-    "&reset=1";
+    "&reset=1&botaoAjuda=1";
 
   const buffer = await captureWebsite.buffer(mazeGameUrl, {
-    width: 1283,
-    height: 638,
+    width: 577,
+    height: 556,
   });
 
-  const { thumbnailName, thumbnailUrl } = await upFireThumbnail(buffer);
+  await sharp(buffer)
+    .resize({ width: 700, height: 600, fit: "fill" })
+    .toBuffer()
+    .then((data) => {
+      bufferNewDimension = data;
+    });
+
+  const { thumbnailName, thumbnailUrl } = await upFireThumbnail(
+    bufferNewDimension
+  );
 
   return { thumbnailName, thumbnailUrl };
 };
